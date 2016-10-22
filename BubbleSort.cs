70d0adac6187e69ab84d6331be5713661f173252
GiridharanNair <giridharan066@gmail.com>
2016-10-22 12:50:18 -0600

    Add files via upload

---
+++ BubbleSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Solution
+    {
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            int noOfSwaps = 0;
+            for (int i = 0; i < n; i++)
+            {
+                // Track number of elements swapped during a single array traversal
+                int numberOfSwaps = 0;
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    // Swap adjacent elements if they are in decreasing order
+                    if (a[j] > a[j + 1])
+                    {
+                        swap(ref a[j], ref a[j + 1]);
+                        numberOfSwaps++;
+                    }
+                }
+
+                // If no elements were swapped during a traversal, array is sorted
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+                noOfSwaps += numberOfSwaps;
+            }
+
+            Console.WriteLine("Array is sorted in {0} swaps.", noOfSwaps);
+            Console.WriteLine("First Element: " + a[0]);
+            Console.WriteLine("Last Element: " + a[n - 1]);
+        }
+
+        public static void swap(ref int a, ref int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+    }
+}
