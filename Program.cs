a32df59f34425d640a6f8154b5e4a873135da682
Giridharan Rajagopalan <giridharan066@gmail.com>
2017-04-12 21:04:20 -0500

    Binary Search - Adding Items and Finding its Sum
    Binary Search - Adding Items and Finding its Sum

--- Program.cs
+++ Program.cs
@@ -1,112 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleApplication1
 {
     class Program
     {
-        static char[] chararray;
-        /// <summary>
-        /// Week of 24 challenge- Hello LadyBug
-        /// </summary>
-        /// <param name="args"></param>
+        static int leftSum,rightSum,total;
+
+
         static void Main(string[] args)
         {
-            
-            int Q = Convert.ToInt32(Console.ReadLine());
-            for (int a0 = 0; a0 < Q; a0++)
-            {
-                int n = Convert.ToInt32(Console.ReadLine());
-                string b = Console.ReadLine();
-                bool result = SatisfyLady(n, b);
-                foreach (char c in chararray)
-                {
-                    Console.Write(c);
-                }                
-            }         
-        }
-        /// <summary>
-        /// Checks for the movement is possible or not
-        /// </summary>
-        /// <param name="length"></param>
-        /// <param name="word"></param>
-        /// <returns>oolean value</returns>
-        static bool SatisfyLady(int length, string word)
-        {
-            chararray = word.ToCharArray();
-            int count = 0;
-            for(int i=0;i< chararray.Length-1; i++)
-            {
-                if (chararray[i].Equals('_'))
-                {
-                    count += 1;
-                    swapunderscores(ref chararray[i], ref chararray[i+1], i);
-                }
-                if (word.Count(x => x == chararray[i]) <= 1)
-                {
-                    Console.WriteLine("Not a Happy Bug");
-                }
-                
-            }
-            if (count == 0)
-            {
-                int counterLength = 0;
-                for(int k=0;k< chararray.Length-1; k++)
-                {
-                    if (chararray[k].Equals(chararray[k + 1]))
-                    {
-                        counterLength += 1;                             
-                    }
-                    else if (counterLength > 0)
-                    {
-                        Console.WriteLine(chararray[k] + " is a happy bug, I will check others");
-                        counterLength = 0;
-                    }
-                    else { 
-                        Console.WriteLine("No Happy Bug");
-                        return false;
-                    }
-
-                }
-               
-            }
-
-            
-            return true;
+            CreateMyBinaryTree();
         }
 
-        /// <summary>
-        /// Swaps underscores with the next item and swaps that item with the matching item.
-        /// </summary>
-        /// <param name="itemIndex"></param>
-        /// <param name="nextItemIndex"></param>
-        /// <param name="indexOfUnderScore"></param>
-        static void swapunderscores(ref char itemIndex, ref char nextItemIndex, int indexOfUnderScore)
+        private static void CreateMyBinaryTree()
         {
-            char temp= itemIndex;
-            itemIndex=nextItemIndex;
-            nextItemIndex =temp;
-            swapletters(ref itemIndex, indexOfUnderScore);
-        }
-        /// <summary>
-        /// Swaps the color item on the board to the matching color item if it exists.
-        /// </summary>
-        /// <param name="itemIndex"></param>
-        /// <param name="index"></param>
-        private static void swapletters(ref char itemIndex, int index)
-        {
-            for(int j = 0; j < index; j++)
+            int[] items = new int[8] {6,13,2,3,9,10,4,5};
+            Node addItems = new Node(7);
+            foreach(int no in items)
             {
-                if (chararray[j].Equals(itemIndex))
-                {
-                    char temp = chararray[j+1];
-                    chararray[j + 1]= itemIndex;
-                    chararray[index] = temp;    
-                            
-                }
+                addItems.addBinaryNodes(no);
+            }
+            int sum = getSumOfNodes(addItems);
+            Console.WriteLine(sum);
+            Console.ReadLine();   
+        }
+
+        private static int getSumOfNodes(Node currentNode)
+        {
+            if (currentNode == null)
+            {
+                return 0;
+            }
+            //else if(currentNode.left==null && currentNode.right == null)
+            //{
+            //    return 0;
+            //}
+            else
+            {
+                leftSum = getSumOfNodes(currentNode.left);
+                rightSum = getSumOfNodes(currentNode.right);
+                total = currentNode.value + leftSum + rightSum;
+                return total;
             }
         }
-        
     }
+
+
+    class Node
+    {
+        public Node left;
+        public Node right;
+        public int value;
+
+        public Node()
+        {
+
+        }
+        public Node(int num)
+        {
+            this.value = num;
+            this.left = null;
+            this.right = null;
+
+        }
+
+        public void addBinaryNodes(int num)
+        {
+            // Left Section
+            if(num< this.value)
+            {
+                if (this.left != null)
+                {
+                    this.left.addBinaryNodes(num);
+                }
+                else
+                {
+                    this.left = new Node(num);
+                }
+            }
+            // Right Section
+            else if(num>this.value)
+            {
+                if (this.right != null)
+                {
+                    this.right.addBinaryNodes(num);
+                }
+                else
+                {
+                    this.right = new Node(num);
+                }
+            }
+        }
+
+    }
+
 }
